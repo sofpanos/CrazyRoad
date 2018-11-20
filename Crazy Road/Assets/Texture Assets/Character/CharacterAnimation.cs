d674ba1521085e1c631697603fc563612ef19999
sofpanos <sofpanos@hotmail.com>
2018-11-20 10:46:59 +0200

    Ground Completed, Imported Character Sprites

---
+++ Crazy Road/Assets/Texture Assets/Character/CharacterAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour {
+	public List<Sprite> FaceBack = new List<Sprite>();
+	public List<Sprite> FaceForward = new List<Sprite>();
+	public List<Sprite> FaceRight = new List<Sprite>();
+	public List<Sprite> FaceLeft = new List<Sprite>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
